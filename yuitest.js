39a9b4f2721f00b19f5f6fad508d7c13b4ac7ba7
Darren Schnare <schnare.d@gmail.com>
2013-11-17 13:43:02 -0500

    Remove the yuitest script

--- yuitest.js
+++
@@ -1,7 +0,0 @@
-require('yuicompressor').compress('./test/support/web/css/src/a.css', {}, function (error, data, extra) {
-  if (error) {
-    throw error;
-  } else {
-    console.log(data);
-  }
-});
